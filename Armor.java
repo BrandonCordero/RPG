c4060a293058a978a8b53287dccaaf28c396e418
https://github.com/new <bmcordero@abany.edu>
2016-09-17 21:14:58 -0400

    Added my Constructor and methods to the Armor class

--- Armor.java
+++ Armor.java
@@ -1,5 +1,22 @@
 // creates a sub-class called Armor from the Super class Item
-public class Armor extends Item {
+public class Armor extends Item{
   // properties of armor class
   private int defense; 
+  
+  // constructor
+  public Armor (int weight,  
+                String name,
+                int defense) {
+    super(name, weight);
+    this.defense = defense;
+  }
+    // methods getters/setters
+  public int getDefense() {
+    return this.defense;
+  }
+  
+  public void setDefense(int defense) {
+    this.defense = defense;
+  }
+   
 }
