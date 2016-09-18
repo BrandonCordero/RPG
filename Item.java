ef733dfda0ef7b7a04a9a3f69d303149662cfeec
https://github.com/new <bmcordero@abany.edu>
2016-09-17 21:12:11 -0400

    Added my methods to the Item class (Getters and Setters)

--- Item.java
+++ Item.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 // Creating Item Class
-private class Item {
+public class Item {
   
   // properties 
   private String name;
@@ -11,6 +11,29 @@ private class Item {
     this.name = name;
     this.weight = weight;
   }
+  // Methods:
 
+  // getters/setters
+   public String getName() {
+    return this.name;
+  }
+  
+  public int getWeight() {
+    return this.weight;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public void setWeight(int weight) {
+    this.weight = weight;
+  }
+  
+//  prints discription of item (name and weight)
+  public void examine()
+  {
+    System.out.println("Name: "+this.name);
+    System.out.println("Weight: "+weight);
+  }
 }
   
