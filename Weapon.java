5dd5a127fb177372de9a49f03c241baf4b686e89
https://github.com/new <bmcordero@abany.edu>
2016-09-17 21:14:14 -0400

    Added my Constructor and methods to the Weapon class

--- Weapon.java
+++ Weapon.java
@@ -1,5 +1,21 @@
 // creates a sub-class called Weapon from the Super class Item
 public class Weapon extends Item {
   // properties of weapon class
-  private int defense;
+  private int damage;
+   // constructor
+  public Weapon (int weight,  
+                String name,
+                int damage) {
+    super(name, weight);
+    this.damage = damage;
+  }
+    // methods getters/setters
+  public int getDamage() {
+    return this.damage;
+  }
+  
+  public void setDefense(int damage) {
+    this.damage = damage;
+  }
+   
 }
