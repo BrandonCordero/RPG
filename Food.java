3838ca630379413534a5be187b41f95575a1a6b4
https://github.com/new <bmcordero@abany.edu>
2016-09-17 21:17:41 -0400

    Created the constructosr and methods for the Food class

--- Food.java
+++ Food.java
@@ -3,4 +3,31 @@ public class Food extends Item {
   // properties of food class
   private int nutrition;
   private int quantity;
+  // constructor
+  public Food (int weight,  
+                String name,
+                int nutrition,
+                int quantity) {
+    super(name, weight);
+    this.nutrition = nutrition;
+    this.quantity = quantity;
+  }
+     // methods getter/setters
+  
+  public int getNutrition() {
+    return this.nutrition;
+  }
+  
+  public void setNutrition(int nutrition) {
+    this.nutrition = nutrition;
+  }
+public int getQuantity() {
+    return this.quantity;
+  }
+  
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
 }
+  
+  
